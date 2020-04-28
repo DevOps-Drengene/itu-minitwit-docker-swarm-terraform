b69d6706e4f9297f4419b69594da26501465d4b8
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2020-04-28 20:56:45 +0200

    Add reference to permenant floating IP (no manage)

--- ip.tf
+++ ip.tf
@@ -1,12 +1,13 @@
-resource "digitalocean_floating_ip" "public-ip" {
-  region = var.region
+# Not managed by Terraform (just a reference to existing resource)
+data "digitalocean_floating_ip" "public-ip" {
+  ip_address = "157.245.25.142"
 }
 
 resource "digitalocean_floating_ip_assignment" "public-ip" {
-  ip_address = digitalocean_floating_ip.public-ip.ip_address
+  ip_address = data.digitalocean_floating_ip.public-ip.ip_address
   droplet_id = digitalocean_droplet.minitwit-swarm-leader.id
 }
 
 output "public_ip" {
-  value = digitalocean_floating_ip.public-ip.ip_address
+  value = data.digitalocean_floating_ip.public-ip.ip_address
 }
